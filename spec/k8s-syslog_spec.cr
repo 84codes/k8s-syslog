cb7e515a1c74aa2371101226f132f9c2b6ccb21d
Carl Hörberg <carl@84codes.com>
2022-05-23 20:45:33 +0200

    make specs pass

--- spec/k8s-syslog_spec.cr
+++ spec/k8s-syslog_spec.cr
@@ -3,8 +3,8 @@ require "../src/k8s-syslog/syslog"
 
 describe K8sSyslog::Syslog do
   it "produces a line" do
-    line = K8sSyslog::Syslog.line "hostname", "component", "message", Time.unix(0)
-    line.should eq "<142>1 1970-01-01T00:00:00Z hostname component - - - message\n"
+    line = K8sSyslog::Syslog.line "my-app-abc123", "my-app", "message", Time.unix(0)
+    line.should eq "<142>1 1970-01-01T00:00:00Z my-app abc123 - - - message\n"
   end
 
   it "prints as is to io" do
